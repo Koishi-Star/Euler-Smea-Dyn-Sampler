2e781ddf1e2f28f94a24918e281161bdc0b92f3a
Pam <pamhome21@gmail.com>
2024-04-10 18:00:08 +0500

    inpaint fix

--- sampling.py
+++ sampling.py
@@ -1,3 +1,22 @@
+class _MaskRescaler:
+    def __init__(self, model, x, mode='nearest-exact'):
+        self.model = model
+        self.x = x
+        self.mode = mode
+        self.init_latent, self.mask, self.nmask = model.init_latent, model.mask, model.nmask
+
+    def __enter__(self):
+        if self.init_latent is not None:
+            self.model.init_latent = torch.nn.functional.interpolate(input=self.init_latent, size=self.x.shape[2:4], mode=self.mode)
+        if self.mask is not None:
+            self.model.mask = torch.nn.functional.interpolate(input=self.mask.unsqueeze(0), size=self.x.shape[2:4], mode=self.mode).squeeze(0)
+        if self.nmask is not None:
+            self.model.nmask = torch.nn.functional.interpolate(input=self.nmask.unsqueeze(0), size=self.x.shape[2:4], mode=self.mode).squeeze(0)
+
+    def __exit__(self, type, value, traceback):
+        del self.model.init_latent, self.model.mask, self.model.nmask
+        self.model.init_latent, self.model.mask, self.model.nmask = self.init_latent, self.mask, self.nmask
+
 @torch.no_grad()
 def dy_sampling_step(x, model, dt, sigma_hat, **extra_args):
 
@@ -16,15 +35,10 @@ def dy_sampling_step(x, model, dt, sigma_hat, **extra_args):
     a_list = x.unfold(2, 2, 2).unfold(3, 2, 2).contiguous().view(batch_size, 4, m * n, 2, 2)
     c = a_list[:, :, :, 1, 1].view(batch_size, 4, m, n)
 
-    # Please note that this is just a temporary solution and doesn't actually resolve the issue.
-    try:
-        # print('test before denoised')
+    with _MaskRescaler(model, c):
         denoised = model(c, sigma_hat * c.new_ones([c.shape[0]]), **extra_args)
-        d = to_d(c, sigma_hat, denoised)
-        c = c + d * dt
-    except Exception as e:
-        # print('can not denoised. Using original Euler method.')
-        return x
+    d = to_d(c, sigma_hat, denoised)
+    c = c + d * dt
 
     d_list = c.view(batch_size, 4, m * n, 1, 1)
     a_list[:, :, :, 1, 1] = d_list[:, :, :, 0, 0]
@@ -72,13 +86,12 @@ def sample_euler_dy(model, x, sigmas, extra_args=None, callback=None, disable=No
 @torch.no_grad()
 def smea_sampling_step(x, model, dt, sigma_hat, **extra_args):
     m, n = x.shape[2], x.shape[3]
-    x = torch.nn.functional.interpolate(input=x, size=None, scale_factor=(1.25, 1.25), mode='nearest',
-                                    align_corners=None,recompute_scale_factor=None)
-    denoised = model(x, sigma_hat * x.new_ones([x.shape[0]]), **extra_args)
+    x = torch.nn.functional.interpolate(input=x, scale_factor=(1.25, 1.25), mode='nearest-exact')
+    with _MaskRescaler(model, x):
+        denoised = model(x, sigma_hat * x.new_ones([x.shape[0]]), **extra_args)
     d = to_d(x, sigma_hat, denoised)
     x = x + d * dt
-    x = torch.nn.functional.interpolate(input=x, size=(m,n), scale_factor=None, mode='nearest',
-                                        align_corners=None, recompute_scale_factor=None)
+    x = torch.nn.functional.interpolate(input=x, size=(m,n), mode='nearest-exact')
     return x
 
 @torch.no_grad()
@@ -98,9 +111,9 @@ def sample_euler_smea_dy(model, x, sigmas, extra_args=None, callback=None, disab
         # Euler method
         x = x + d * dt
         if sigmas[i + 1] > 0:
-            if i + 1 // 2 == 1:
+            if (i + 1) // 2 == 1:
                 x = dy_sampling_step(x, model, dt, sigma_hat, **extra_args)
-            if i + 1 // 2 == 0:
+            if (i + 1) // 2 == 0:
                 x = smea_sampling_step(x, model, dt, sigma_hat, **extra_args)
         if callback is not None:
             callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
